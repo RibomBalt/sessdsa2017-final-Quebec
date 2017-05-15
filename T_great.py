c99234a581b2588669d8ed9f7560260fd00d3f13
Cobalt Yang <yangfan1@pku.edu.cn>
2017-05-15 23:26:29 +0800

    补完文档部分

--- T_great.py
+++ T_great.py
@@ -5,12 +5,12 @@ from table import *
 
 # 当前table是假的！是为了阻止报错放的一个旧版table！
 
-def play(tb:TableData, ds:dict):
+def play(tb:TableData, ds:dict) -> RacketAction:
     '''
     主函数，每次游戏进程会调用这个函数
-    :param tb: 桌面数据，
-    :param ds: 
-    :return: 
+    :param tb: TableData，桌面数据情况。
+    :param ds: dict，临时存储数据的字典
+    :return: RacketAction，保存了这次的球拍移动的四项信息。
     '''
     # return RacketAction(tb.tick, tb.ball['position'].y - tb.side['position'].y, 0, 0)
     pass

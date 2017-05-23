b3a8895246166200655956630d0d010da7255e7d
Cobalt Yang <yangfan1@pku.edu.cn>
2017-05-23 13:56:49 +0800

    完成Sider

--- T_Sider.py
+++ T_Sider.py
@@ -1,17 +1,39 @@
 from table import *
 
+def ball_v_range(y):
+    """
+    根据我方出射点坐标，算出y轴可取速度的边界值
+    :param tb.step: 1800 tick
+    :param Y: 镜像点y坐标
+    :param height: 乒乓球桌的宽度, DIM[3] - DIM[2]
+    :return: 与桌碰撞次数
+    """
+    height = DIM[3] - DIM[2]
+    # v0,v1,v2,v3是速度的范围边界:v0-v1,v2-v3可取
+    v0 = (3 * height - y) // 1800
+    v1 = (1 * height - y) // 1800
+    v2 = (0 - y) // 1800
+    v3 = (-2 * height - y) // 1800
+    return v0, v1, v2, v3
+
 # 发球函数，总是做为West才发球
 # ds为函数可以利用的存储字典
 # 函数需要返回球的y坐标，和y方向的速度
 def serve(ds:dict) -> tuple:
-    return BALL_POS[1], BALL_V[1]
+    return BALL_POS[1], ball_v_range(BALL_POS[1])[1] + 1
 
 # 打球函数
 # tb为TableData类型的对象
 # ds为函数可以利用的存储字典
 # 函数需要返回一个RacketAction对象
 def play(tb:TableData, ds:dict) -> RacketAction:
-    return RacketAction(tb.tick, tb.ball['position'].y - tb.side['position'].y, 0, 0, None, None)
+    v_range = ball_v_range(tb.ball['velocity'].y)
+    target = min(v_range, key=lambda x:abs(x - tb.ball['velocity'].y)) - tb.ball['velocity'].y
+    if tb.cards['cards']:
+        side, item = 'SELF', tb.cards['cards'].pop(0)
+    else:
+        side, item = None, None
+    return RacketAction(tb.tick, tb.ball['position'].y - tb.side['position'].y, target, 500000 - tb.ball['position'].y, side, item)
 
 # 对局后保存历史数据函数
 # ds为函数可以利用的存储字典
